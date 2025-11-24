ca2f277f8629abb98b1340fbefd30d64a826a651
Nasrullah Khan <nasrukhan2070@gmail.com>
2025-11-25 00:40:24 +0500

    Introduce app namespaces (major, company, products) and update all templates to namespaced URL tags

--- Poultry/urls.py
+++ Poultry/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('major.urls')),
-    path('', include('company.urls')),
-    path('', include('products.urls')),
+    path('', include(('major.urls', 'major'), namespace='major')),
+    path('', include(('company.urls', 'company'), namespace='company')),
+    path('', include(('products.urls', 'products'), namespace='products')),
 ]
