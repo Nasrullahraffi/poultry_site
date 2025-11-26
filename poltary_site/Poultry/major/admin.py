6d449175db223764edb54210bfd49539b719ae55
Nasrullah Khan <nasrukhan2070@gmail.com>
2025-11-27 01:36:58 +0500

    UPDT: handled all the forms for appearance and site completion

--- poltary_site/Poultry/major/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from major.models import *
-# Register your models here.
-
-admin.site.register(Breeder_Model)
-admin.site.register(Distributed_among_companies)
--- poltary_site/Poultry/products/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from products import views
-urlpatterns = [
-    path('feed/', views.FeedView, name="feed"),
-    path('medicine/', views.MedicineView, name="medicines"),
-    path('disease/', views.DiseaseView, name="diseases"),
-]
